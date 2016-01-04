e21a8c474796cc726ed49c8bb2ce33beb404d126
lawn.cao <lawn.cao@vipshop.com>
2016-01-04 12:13:06 +0800

    import reactiveCocoa with Carthage

--- MVVMTest/ViewController.m
+++ MVVMTest/ViewController.m
@@ -10,20 +10,50 @@
 
 @interface ViewController ()
 
+@property (nonatomic, copy  ) NSString *name;
+@property (nonatomic, strong) UITextField *textField;
+@property (nonatomic, strong) RACSignal *validNameSignal;
+@property (nonatomic, strong) UILabel *inputLabel;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+
     [super viewDidLoad];
     
-    self.view.backgroundColor = [UIColor redColor];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:self.textField];
+    [self.view addSubview:self.inputLabel];
+    
+    RAC(self, name) = self.textField.rac_textSignal;
+    RAC(self.inputLabel, text) = [RACObserve(self, name) map:^(NSString *name) {
+        return name;
+    }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (UITextField *)textField{
+    
+    if (_textField == nil) {
+        _textField = [[UITextField alloc] init];
+        _textField.frame = CGRectMake(10, 64, self.view.bounds.size.width-20, 44);
+        _textField.placeholder = @"请输入用户名";
+        _textField.backgroundColor = [UIColor grayColor];
+    }
+    return _textField;
+}
+
+- (UILabel *)inputLabel{
+    
+    if (_inputLabel == nil) {
+        _inputLabel = [[UILabel alloc] init];
+        _inputLabel.frame = CGRectMake(10, 150, self.view.bounds.size.width-20, 44);
+        _inputLabel.backgroundColor = [UIColor grayColor];
+    }
+    return _inputLabel;
 }
 
 @end
